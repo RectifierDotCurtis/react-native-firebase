1670c17673d3e0c7b0397fb04700417027f2f07b
RectifierDotCurtis <79728994+RectifierDotCurtis@users.noreply.github.com>
2021-02-26 20:02:36 -0500

    Black List Start Question Mark
    Hopefully keys work

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAOWHBpPhKoNhcGFKHH_Q_0AtL2gV-imgQ',
-  authDomain: 'production-a9404.firebaseapp.com',
-  databaseURL: 'https://production-a9404.firebaseio.com',
-  projectId: 'production-a9404',
-  storageBucket: 'production-a9404.appspot.com',
-  messagingSenderId: '525472070731',
-  appId: '1:525472070731:web:ee873bd62c0deb7eba61ce',
+  apiKey: 'AIzaSyA7wroY8jsgkOSuy6X-bvUShOsB-Xp1Yvc',
+  authDomain: 'black-list-d22ed.firebaseapp.com',
+ // databaseURL: 'https://production-a9404.firebaseio.com',
+  projectId: 'black-list-d22ed',
+  storageBucket: 'black-list-d22ed.appspot.com',
+  messagingSenderId: '499051785976',
+  appId: '1:499051785976:ios:f64e3f5d975b0bb3df550d',
 };
 
 if (!firebase.apps.length) {
